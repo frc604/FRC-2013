24401da93c967e11ac386a7a8b7d7865ee2b561b
Michael Smith <michael@diglumi.com>
2013-02-11 16:38:32 -0800

    Correct NetBeans filename mangling

--- src/com/_604robotics/robot2013/Robot2013.java
+++ src/com/_604robotics/robot2013/Robot2013.java
@@ -1,41 +1,21 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) FIRST 2008. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package com._604robotics.robot2013;
 
+import com._604robotics.robot2013.modes.TeleopMode;
+import com._604robotics.robot2013.modules.Drive;
+import com._604robotics.robot2013.modules.Rotation;
+import com._604robotics.robotnik.Robot;
+import com._604robotics.robotnik.coordinator.ModeMap;
+import com._604robotics.robotnik.module.ModuleMap;
 
-import edu.wpi.first.wpilibj.SimpleRobot;
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SimpleRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
-public class Robot2013 extends SimpleRobot {
-    /**
-     * This function is called once each time the robot enters autonomous mode.
-     */
-    public void autonomous() {
+public class Robot2013 extends Robot {
+    public Robot2013 () {
+        this.set(new ModuleMap() {{
+            add("Drive", new Drive());
+            add("Rotation", new Rotation());
+        }});
         
-    }
-
-    /**
-     * This function is called once each time the robot enters operator control.
-     */
-    public void operatorControl() {
-
-    }
-    
-    /**
-     * This function is called once each time the robot enters test mode.
-     */
-    public void test() {
-    
+        this.set(new ModeMap() {{
+            setTeleopMode(new TeleopMode());
+        }});
     }
 }
