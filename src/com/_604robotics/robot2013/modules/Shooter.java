83d049b7a76fbd849735fbab2365b24a476ec518
sushimkr <alan@Acer-Alan>
2013-02-12 23:57:58 -0800

    I'm pretty sure this is how you actually do it, based on the fact that all the other modules seem to do it this way

--- src/com/_604robotics/robot2013/modules/Shooter.java
+++ src/com/_604robotics/robot2013/modules/Shooter.java
@@ -4,6 +4,9 @@
  */
 package com._604robotics.robot2013.modules;
 
+import com._604robotics.robotnik.action.Action;
+import com._604robotics.robotnik.action.controllers.ElasticController;
+import com._604robotics.robotnik.action.field.ActionData;
 import com._604robotics.robotnik.module.Module;
 import edu.wpi.first.wpilibj.Victor;
 
@@ -16,13 +19,18 @@ public class Shooter extends Module {
     private final double speed = 0;/*<Speed for shooter goes here*/
     
     public Shooter(){
-        victor.set(0);
-    }
-    
-    public void on(){
-        victor.set(speed);
-    }
-    public void off(){
-        victor.set(0);
+        this.set(new ElasticController() {{
+            addDefault("off", new Action() {
+                public void begin (ActionData data) {
+                    victor.set(0);
+                }
+            });
+            
+            add("on", new Action() {
+                public void begin (ActionData data) {
+                    victor.set(speed);
+                }
+            });
+        }});
     }
 }
