20622b038ee4d968dc917afbdae8e67a0c60f3e2
Michael Smith <michael@diglumi.com>
2013-02-12 17:25:44 -0800

    Document TeleopMode

--- src/com/_604robotics/robot2013/modes/TeleopMode.java
+++ src/com/_604robotics/robot2013/modes/TeleopMode.java
@@ -11,11 +11,14 @@ public class TeleopMode extends Coordinator {
     private final XboxController manipController = new XboxController(2);
     
     public void apply (ModuleManager modules) {
+        // Bind joysticks to Tank Drive
         this.fill(new DataWire(modules.getModule("Drive").getAction("Tank Drive"), "left", driveController.leftStick.Y));
         this.fill(new DataWire(modules.getModule("Drive").getAction("Tank Drive"), "right", driveController.rightStick.Y));
         
+        // Bind manual shooter rotation to manipulator joystick
         this.fill(new DataWire(modules.getModule("Rotation").getAction("Manual"), "power", manipController.leftStick.Y));
         
+        // Bind frisbee stucking to manipulator button
         this.bind(new Binding(modules.getModule("Feeder").getAction("On"), manipController.buttons.LB));
     }
 }
