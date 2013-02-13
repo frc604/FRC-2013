8656d9f8540dcb9e6ad2df6120902718aeb2b8ab
Michael Smith <michael@diglumi.com>
2013-02-12 17:49:49 -0800

    Code cleanup and formatting.
    Also, refactor "PneumaticsRegulator" to "Regulator".

--- src/com/_604robotics/robot2013/modules/Shifter.java
+++ src/com/_604robotics/robot2013/modules/Shifter.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com._604robotics.robot2013.modules;
 
 import com._604robotics.robotnik.action.Action;
@@ -10,16 +6,13 @@ import com._604robotics.robotnik.action.field.ActionData;
 import com._604robotics.robotnik.module.Module;
 import edu.wpi.first.wpilibj.Solenoid;
 
-/**
- *
- * @author Aaron Wang
- */
 public class Shifter extends Module {
+    // FIXME: Get the right port for the Single Action Solenoid.
     private final Solenoid solenoid = new Solenoid(1);
-    //FIXME: Get the right port for the Single Action Solenoid.
+    
     public Shifter () {
-        this.set(new ElasticController(){{
-            addDefault ("Low Power", new Action(){
+        this.set(new ElasticController() {{
+            addDefault("Low Power", new Action() {
                 public void begin (ActionData data) {
                     solenoid.set(false);
                 }
