7324ae6a7581eb8a1c4523e5768fd223af85db83
Michael Smith <michael@diglumi.com>
2013-02-14 16:03:33 -0800

    Invert Drive motors

--- src/com/_604robotics/robot2013/modules/Drive.java
+++ src/com/_604robotics/robot2013/modules/Drive.java
@@ -6,12 +6,18 @@ import com._604robotics.robotnik.action.field.ActionData;
 import com._604robotics.robotnik.action.field.FieldMap;
 import com._604robotics.robotnik.module.Module;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.RobotDrive.MotorType;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Drive extends Module {
     private final RobotDrive drive = new RobotDrive(new Victor(2), new Victor(1));
     
     public Drive () {
+        drive.setInvertedMotor(MotorType.kFrontLeft, true);
+        drive.setInvertedMotor(MotorType.kFrontRight, true);
+        drive.setInvertedMotor(MotorType.kRearLeft, true);
+        drive.setInvertedMotor(MotorType.kRearRight, true);
+        
         this.set(new ElasticController() {{
             addDefault("Tank Drive", new Action(
                     new FieldMap() {{
