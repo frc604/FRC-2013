02537b76763aaf8d764f0669767d1cfed4ee67ae
Michael Smith <mdsmtp@gmail.com>
2013-09-29 23:09:22 -0700

    Make firing presets dashboard-configurable

--- src/com/_604robotics/robot2013/modules/Targets.java
+++ src/com/_604robotics/robot2013/modules/Targets.java
@@ -1,29 +1,15 @@
 package com._604robotics.robot2013.modules;
 
-import com._604robotics.robotnik.data.Data;
 import com._604robotics.robotnik.data.DataMap;
+import com._604robotics.robotnik.data.sources.DashboardData;
 import com._604robotics.robotnik.module.Module;
 
 public class Targets extends Module {
     public Targets () {
         this.set(new DataMap() {{
-            add("Top Angle", new Data() {
-                public double run () {
-                    return -21D;
-                }
-            });
-            
-            add("Middle Angle", new Data() {
-                public double run () {
-                    return -16D;
-                }
-            });
-            
-            add("Bottom Angle", new Data() {
-                public double run () {
-                    return -10D;
-                }
-            });
+            add("Top Angle", new DashboardData("Top Angle", -22D));
+            add("Middle Angle", new DashboardData("Middle Angle", -21D));
+            add("Bottom Angle", new DashboardData("Bottom Angle", -20D));
         }});
     }
 }
