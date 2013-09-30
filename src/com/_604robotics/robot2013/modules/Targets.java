5245e7730c5c615254fc04ccf0bee1263d1c94a1
Michael Smith <mdsmtp@gmail.com>
2013-09-29 22:47:07 -0700

    Adjust firing angle to 21 degrees

--- src/com/_604robotics/robot2013/modules/Targets.java
+++ src/com/_604robotics/robot2013/modules/Targets.java
@@ -9,7 +9,7 @@ public class Targets extends Module {
         this.set(new DataMap() {{
             add("Top Angle", new Data() {
                 public double run () {
-                    return -22D;
+                    return -21D;
                 }
             });
             
