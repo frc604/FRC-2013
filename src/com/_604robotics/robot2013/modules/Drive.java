b3869ca712e509bb20d668672b05516bb255e4b5
Michael Smith <mdsmtp@gmail.com>
2013-10-01 20:15:02 -0700

    Implement Racecar Drive mode

--- src/com/_604robotics/robot2013/modules/Drive.java
+++ src/com/_604robotics/robot2013/modules/Drive.java
@@ -37,6 +37,101 @@ public class Drive extends Module {
                    drive.tankDrive(0D, 0D);
                }
             });
+            
+            add("Racecar Drive", new Action (new FieldMap() {{
+                define("wheel", 0D);
+                define("throttle", 0D);
+                
+                define("powered", false);
+                define("sharp", false);
+            }}) {
+                private double lastWheel;
+                
+                public void begin (ActionData data) {
+                    lastWheel = data.get("wheel");
+                    
+                    drive.tankDrive(0D, 0D);
+                }
+                
+                public void run (ActionData data) {
+                    final boolean powered = data.is("powered");
+                    final boolean sharp   = data.is("sharp");
+                    
+                    double wheel = data.get("wheel");
+                    final double throttle = -data.get("throttle");
+                    
+                    {
+                        wheel  = smoothWheel(wheel, powered);
+                        wheel += calculateInertia(wheel - lastWheel, wheel, powered);
+                    }
+                    
+                    double left = throttle;
+                    double right = throttle;
+
+                    {
+                        if (!sharp) {
+                            wheel = calculateAngularPower(wheel, throttle, powered);
+                        }
+
+                        left  += wheel;
+                        right -= wheel;
+
+                        if (sharp) {
+                            final double _left  = left;
+                            final double _right = right;
+
+                            left  = counterbalance(_left,  _right);
+                            right = counterbalance(_right, _left);
+                        }
+                    }
+                    
+                    drive.tankDrive(limitOutput(left), limitOutput(right));
+                    
+                    lastWheel = wheel;
+                }
+                
+                public void end (ActionData data) {
+                    drive.tankDrive(0D, 0D);
+                }
+                
+                private double smoothWheel (double wheel, boolean powered) {
+                    final int iterations = powered ? 2 : 3;
+                    final double factor = powered ? 0.6 : 0.5;
+                    
+                    for (int i = 0; i < iterations; i++) {
+                        wheel = Math.sin(Math.PI / 2D * factor * wheel) /
+                                Math.sin(Math.PI / 2D * factor);
+                    }
+                    
+                    return wheel;
+                }
+                
+                private double calculateInertia (double delta, double wheel, boolean powered) {
+                    if      (powered               ) return delta * 5D;
+                    else if (wheel * delta   > 0   ) return delta * 2.5;
+                    else if (Math.abs(wheel) > 0.65) return delta * 5D;
+                    else                             return delta * 3D;    
+                }
+                
+                private double calculateAngularPower (double wheel, double throttle, boolean powered) {
+                    return Math.abs(throttle) * wheel * (powered ? 0.85 : 0.75);
+                }
+                
+                private double limitOutput (double output) {
+                    return Math.min(Math.max(output, -1D), 1D);
+                }
+                
+                private double counterbalance (double weight, double counter) {
+                    final double magnitude = Math.abs(counter);
+                    final double polarity = counter / magnitude;
+                    
+                    if (magnitude > 1D) {
+                        return weight - ((magnitude - 1) * polarity);
+                    }
+                    
+                    return weight;
+                }
+            });
         }});
     }
 }
