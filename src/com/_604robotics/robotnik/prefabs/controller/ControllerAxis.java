516892521207fdaa9d17e86d8239958c3fe828eb
Michael Smith <mdsmtp@gmail.com>
2013-10-02 16:55:24 -0700

    Add "factor" option to framework ControllerAxis

--- src/com/_604robotics/robotnik/prefabs/controller/ControllerAxis.java
+++ src/com/_604robotics/robotnik/prefabs/controller/ControllerAxis.java
@@ -8,6 +8,7 @@ public class ControllerAxis implements DataAccess {
     private final int axis;
     
     private double deadband = 0D;
+    private double factor = 1D;
     
     public ControllerAxis (Joystick joystick, int axis) {
         this.joystick = joystick;
@@ -15,12 +16,16 @@ public class ControllerAxis implements DataAccess {
     }
     
     public double get () {
-        final double value = this.joystick.getRawAxis(this.axis);
+        final double value = this.joystick.getRawAxis(this.axis) * this.factor;
         
         return Math.abs(value) < this.deadband ? 0D : value;
     }
     
     public void setDeadband (double deadband) {
         this.deadband = deadband;
+    }
+    
+    public void setFactor (double factor) {
+        this.factor = factor;
     }
 }
