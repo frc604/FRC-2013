0a3f18aafb82b7b45ab302f316352ae6f469066f
Michael Smith <mdsmtp@gmail.com>
2013-10-11 20:12:25 -0700

    Add error handling/warning outputs to Logger

--- src/com/_604robotics/robotnik/utils/Logger.java
+++ src/com/_604robotics/robotnik/utils/Logger.java
@@ -2,11 +2,20 @@ package com._604robotics.robotnik.utils;
 
 public class Logger {
     public static void missing (String type, String name) {
-        System.err.println("WARNING: Missing " + type + " - " + name);
-        new Error().printStackTrace();
+        warn("WARNING: Missing " + type + " - " + name);
     }
     
     public static void log (String message) {
         System.out.println("[INFO] " + message);
+    }
+    
+    public static void warn (String message) {
+        System.err.println("[WARN] " + message);
+        new Error().printStackTrace();
+    }
+    
+    public static void error (String message, Error error) {
+        System.err.println("[ERROR] " + message);
+        error.printStackTrace();
     }
 }
