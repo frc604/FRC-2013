78e7d5a6acca630ce91577ca0b95083dcbf09d4f
Michael Smith <mdsmtp@gmail.com>
2013-10-12 17:27:49 -0700

    Read shooter speed from encoder

--- src/com/_604robotics/robot2013/modules/Shooter.java
+++ src/com/_604robotics/robot2013/modules/Shooter.java
@@ -4,18 +4,33 @@ import com._604robotics.robotnik.action.Action;
 import com._604robotics.robotnik.action.controllers.ElasticController;
 import com._604robotics.robotnik.action.field.ActionData;
 import com._604robotics.robotnik.action.field.FieldMap;
+import com._604robotics.robotnik.data.Data;
+import com._604robotics.robotnik.data.DataMap;
 import com._604robotics.robotnik.module.Module;
 import com._604robotics.robotnik.trigger.Trigger;
 import com._604robotics.robotnik.trigger.TriggerMap;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.Victor;
 
 public class Shooter extends Module {
     private final Victor victor = new Victor(4);
+    private final Encoder encoder  = new Encoder(4, 5);
     
     private boolean charged;
     
     public Shooter(){
+        encoder.setDistancePerPulse(1D);
+        encoder.start();
+        
+        this.set(new DataMap() {{
+            add("Shooter Speed", new Data() {
+                public double run () {
+                    return encoder.getRate();
+                }
+            });
+        }});
+        
         this.set(new TriggerMap() {{
             add("Charged", new Trigger() {
                 public boolean run () {
