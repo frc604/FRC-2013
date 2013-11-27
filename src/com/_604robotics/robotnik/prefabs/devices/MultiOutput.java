7d407f25e98391143ffd92a7f0dbf85418c7acc8
Michael Smith <mdsmtp@gmail.com>
2013-11-27 15:14:14 -0800

    MultiVictor -> MultiOutput
    Make this class more general, so it can be used with eg. Talons as well.
    Switch from referencing the Victor class directly to anything that
    implements the PIDOutput interface.

---
+++ src/com/_604robotics/robotnik/prefabs/devices/MultiOutput.java
@@ -0,0 +1,21 @@
+package com._604robotics.robotnik.prefabs.devices;
+
+import edu.wpi.first.wpilibj.PIDOutput;
+
+public class MultiOutput implements PIDOutput {
+    private final PIDOutput[] outputs;
+    
+    public MultiOutput (PIDOutput[] outputs) {
+        this.outputs = outputs;
+    }
+    
+    public void set (double value) {
+        for (int i = 0; i < this.outputs.length; i++) {
+            this.outputs[i].pidWrite(value);
+        }
+    }
+
+    public void pidWrite (double value) {
+        this.set(value);
+    }    
+}
